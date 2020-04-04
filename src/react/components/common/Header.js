5bd79c117ebaf91e6774e61c7c99d352e734fecd
James Spence <Jamesaspence20@gmail.com>
2020-04-04 13:09:51 -0500

    conditionally load buttons in header based on auth

--- src/react/components/common/Header.js
+++ src/react/components/common/Header.js
@@ -2,19 +2,42 @@ import React from 'react';
 import './Header.scss';
 import logo from '../../../resources/logo.svg';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { STATUSES } from '../../../redux/actions';
 
-const Header = () => (
-  <header className="Header">
-    <div className="Header__left">
-      <Link to="/" className="Link--with-logo">
-        <img src={logo} alt="Turniptime logo" className="Header__logo"/>
-      </Link>
-    </div>
-    <div className="Header__right">
+const renderButtons = userStatus => {
+  if ([STATUSES.NOT_LOADED, STATUSES.LOADING].includes(userStatus)) {
+    return <Link to="/" disabled className="Link button is-dark is-loading" />;
+  }
+
+  if (userStatus === STATUSES.SUCCESS) {
+    return <Link to="/price" className="Link button is-dark">Record Price</Link>;
+  }
+
+  return (
+    <>
       <Link to="/login" className="Link button is-dark">Login</Link>
       <Link to="/register" className="Link button is-dark">Sign Up</Link>
-    </div>
-  </header>
-);
+    </>
+  );
+};
+
+const Header = () => {
+  const selectUserStatus = state => state.auth.status;
+  const userStatus = useSelector(selectUserStatus);
+
+  return (
+    <header className="Header">
+      <div className="Header__left">
+        <Link to="/" className="Link--with-logo">
+          <img src={logo} alt="Turniptime logo" className="Header__logo"/>
+        </Link>
+      </div>
+      <div className="Header__right">
+        { renderButtons(userStatus) }
+      </div>
+    </header>
+  );
+};
 
 export default Header;
