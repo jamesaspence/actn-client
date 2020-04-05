30f17f4bf48f10bb635751e54a0a0ae6859afc0e
James Spence <Jamesaspence20@gmail.com>
2020-04-04 21:03:52 -0500

    removed record price link on home page

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './Home.scss';
 import ContentLayout from '../components/common/ContentLayout';
 import PageHeader from '../components/common/PageHeader';
-import { Link } from 'react-router-dom';
 
 const Home = () => (
   <ContentLayout className="Home">
     <PageHeader>Home</PageHeader>
     <p>WIP - please check back again soon!</p>
     <p>For now, please feel free to record your prices - the price data is currently being stored and will be displayed once we finish development on this page.</p>
-    <Link to="/price" className="Home__link button is-link">Record Price</Link>
   </ContentLayout>
 );
 
