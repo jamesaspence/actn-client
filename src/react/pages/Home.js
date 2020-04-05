308eb67210a43bc5b4d19ea2dece876445c14b1c
James Spence <Jamesaspence20@gmail.com>
2020-04-05 15:01:42 -0500

    disabled TopTenContainer on home page (for now)

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ContentLayout from '../components/common/ContentLayout';
 import PageHeader from '../components/common/PageHeader';
 import HomePurchasingContainer from '../components/purchase/HomePurchasingContainer';
-import TopTenContainer from '../components/price/data/TopTenContainer';
 
 const Home = () => {
   const today = new Date();
@@ -14,9 +13,8 @@ const Home = () => {
       <p>WIP - please check back again soon!</p>
       <p>For now, please feel free to record your prices - the price data is currently being stored and will be displayed once we finish development on this page.</p>
       {
-        isSunday ?
-        <HomePurchasingContainer /> :
-        <TopTenContainer />
+        isSunday &&
+        <HomePurchasingContainer/>
       }
     </ContentLayout>
   );
