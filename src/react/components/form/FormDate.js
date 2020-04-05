dc8f39c202a9aba4a38947932e7d9ec7c1cc5322
James Spence <Jamesaspence20@gmail.com>
2020-04-04 23:31:24 -0500

    moved formatDate into date service

--- src/react/components/form/FormDate.js
+++ src/react/components/form/FormDate.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import './FormDate.scss';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
-
-const formatDate = date => {
-  const year = '' + date.getFullYear();
-  let month = '' + (date.getMonth() + 1);
-  let day = '' + date.getDate();
-
-  if (month.length < 2) {
-    month = '0' + month;
-  }
-
-  if (day.length < 2) {
-    day = '0' + day;
-  }
-
-  return `${year}-${month}-${day}`;
-};
+import { formatDate } from '../../../service/date';
 
 const FormDate = ({
   name,
---
+++ src/service/date.js
@@ -0,0 +1,15 @@
+export const formatDate = date => {
+  const year = '' + date.getFullYear();
+  let month = '' + (date.getMonth() + 1);
+  let day = '' + date.getDate();
+
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+
+  return `${year}-${month}-${day}`;
+};
