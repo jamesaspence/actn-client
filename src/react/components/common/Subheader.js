8209d43cd55e070eea8d27c785af9adc767fb8f3
James Spence <Jamesaspence20@gmail.com>
2020-04-05 15:06:31 -0500

    additional styling on Subheaders

--- src/react/components/common/Subheader.js
+++ src/react/components/common/Subheader.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Subheader.scss';
 
 const Subheader = ({ children }) => (
-  <h2 className="subtitle">{ children }</h2>
+  <h2 className="Subheader subtitle">
+    <span className="Subheader__inner">{ children }</span>
+  </h2>
 );
 
 export default Subheader;
