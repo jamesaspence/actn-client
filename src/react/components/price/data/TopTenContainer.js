e70ba4ffb53ebe7dc44da295d58f027e8aec1d1a
James Spence <Jamesaspence20@gmail.com>
2020-04-04 23:54:18 -0500

    retrieving top ten data & rendering table

--- src/react/components/price/data/TopTenContainer.js
+++ src/react/components/price/data/TopTenContainer.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import TopTenPrices from './TopTenPrices';
+import Subheader from '../../common/Subheader';
 import { formatDate } from '../../../../service/date';
 import { fetchTopTenPrices } from '../../../../api/data';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchTopTen } from '../../../../redux/actions/prices';
 import { STATUSES } from '../../../../redux/actions';
 
 const TopTenContainer = () => {
-  const date = new Date();
-  const currentDate = formatDate(date);
-  const currentTime = (date.getHours() > 10 && date.getMinutes() > 4) || date.getHours() >= 12 ? 'evening' : 'morning';
-
   const [ dataRetrieved, setRetrieved ] = useState(false);
   const dispatch = useDispatch();
+
+  const selectTopTenStatus = state => state.prices.topTen.status;
+  const selectTopTenPrices = state => state.prices.topTen.data;
+
+  const topTenStatus = useSelector(selectTopTenStatus);
+  const prices = useSelector(selectTopTenPrices);
+
   useEffect(() => {
     if (dataRetrieved) {
       return;
@@ -29,11 +33,20 @@ const TopTenContainer = () => {
       });
   }, [ dataRetrieved, setRetrieved, dispatch ]);
 
+  const date = new Date();
+  const currentDate = formatDate(date);
+  const currentTime = (date.getHours() > 10 && date.getMinutes() > 4) || date.getHours() >= 12 ? 'evening' : 'morning';
+
   return (
-    <TopTenPrices
-      currentDate={currentDate}
-      currentTime={currentTime}
-    />
+    <div className="TopTenContainer">
+      <Subheader>Top Ten</Subheader>
+      <TopTenPrices
+        currentDate={currentDate}
+        currentTime={currentTime}
+        status={topTenStatus}
+        prices={prices}
+      />
+    </div>
   );
 };
 
--- src/react/components/price/data/TopTenPrices.js
+++ src/react/components/price/data/TopTenPrices.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './TopTenPrices.scss';
+import { STATUSES } from '../../../../redux/actions';
 
-const TopTenPrices = ({ currentDate, currentTime }) => (
-  <div className="TopTenPrices">
-    <h2 className="subtitle">Top Ten</h2>
-    <p>The top ten prices for the {currentTime} of {currentDate} are:</p>
-  </div>
-);
+const renderTable = prices => {
+  if (prices.length < 1) {
+    return <div className="TopTenPrices__table">
+      <small className="Table__info">No data to show</small>
+    </div>
+  }
+
+  return (
+    <table className="table">
+      <thead>
+      <tr>
+        <th><abbr title="Number">#</abbr></th>
+        <th><abbr title="Username">Username</abbr></th>
+        <th><abbr title="Price (Bells)">Price (Bells)</abbr></th>
+      </tr>
+      </thead>
+      <tbody>
+        {prices.map((price, i) => (
+          <tr key={i}>
+            <th>{i + 1}</th>
+            <td>{price.user.username}</td>
+            <td>{price.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+};
+
+const TopTenPrices = ({ currentDate, currentTime, status, prices }) => {
+  if (status == null || status === STATUSES.LOADING) {
+    return (
+      <div className="TopTenPrices">
+        Loading...
+      </div>
+    );
+  }
+
+  return (
+    <div className="TopTenPrices">
+      <p>The top ten prices for the {currentTime} of {currentDate} are:</p>
+      { renderTable(prices) }
+    </div>
+  )
+};
 
 export default TopTenPrices;
